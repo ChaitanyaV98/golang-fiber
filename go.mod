46a337f6e1c19158a8b78d578bfca3a1cf0bab99
ChaitanyaV98 <chaitanya@betalectic.com>
2021-04-08 17:12:55 +0530

    second-commit

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/chaitanyav98/golang-fiber
 
 go 1.16
 
-require github.com/gofiber/fiber v1.14.6 // indirect
+require github.com/gofiber/fiber v1.14.6
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (	
-"github.com/chaitanyav98/go-fiber/book"
+"github.com/chaitanyav98/golang-fiber/book"
 "github.com/gofiber/fiber"
+"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 //import ("goland-crud/book")
 
@@ -16,11 +18,24 @@ func setupRoutes(app *fiber.App){
 	app.Post("/api/v1/book", book.NewBook)
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
+
 func main(){
 	app:= fiber.New()
 
 	setupRoutes(app);
 	app.Listen(4000)
+	defer database.DBConn.Close()
 
 }
 
